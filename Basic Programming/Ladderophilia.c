ab7464e8075787480e6585d7aadfcd0dd3d7e0c3
Dipayan Saha <51865387+richik500@users.noreply.github.com>
2021-03-14 09:44:03 +0530

    Create Ladderophilia.c

---
+++ Basic Programming/Ladderophilia.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+	int n;
+	scanf("%d",&n);
+	printf("*   *\n\n*   *\n\n");
+	while(n--)
+	{
+		printf("*****\n\n");
+		printf("*   *\n\n*   *\n\n");
+	}
+	return 0;
+}
